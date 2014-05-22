cc4ee2aca2e43ca87a62ba1167e999c3587bd96a
Rafael Khan <raf.khan.f@gmail.com>
2014-05-22 14:04:34 -0400

    Added ajax function and test index

--- src/xtraktor.js
+++ src/xtraktor.js
@@ -8,11 +8,35 @@
   // Get metadata from HTML string
   module.fromHtml = function(html) {
 
-    var parseImgTags = function() {
+    var parseImgTags = function(data) {
       var re = /<img.+?src=[\"'](.+?)[\"'].*?>/ig;
-      console.log(re.exec(html));
+      console.log(re.exec(data));
     };
+
+    ajaxGet('http://google.com', parseImgTags);
     
   };
 
+  var ajaxGet = function(url, cb) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400){
+        var resp = request.responseText;
+        cb(resp);
+      } else {
+        console.log('error');
+      }
+    };
+
+    request.onerror = function() {
+      console.log('error');
+    };
+
+    request.send();
+  };
+
 })(window.xtraktor = window.xtraktor || {});
+
+xtraktor.fromHtml("<html><img src='asd' />");
